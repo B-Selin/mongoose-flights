0fd6becb70e92328e497d44b3d51745e413d526f
B-Selin <selin@bozkurt.ca>
2023-07-05 20:27:02 -0600

    new flight function

--- controllers/flights.js
+++ controllers/flights.js
@@ -2,12 +2,23 @@ const Flight = require('../models/flight');
 
 module.exports = {
   new: newFlight,
-  // create,
-  // index
+  create,
+  index
 };
 
 function newFlight (req, res) {
   res.render('flights/new', {
     errorMsg: 'Error, flight not created!' 
   });
+}
+
+async function create(req, res) {
+
+}
+
+async function index(req, res){
+  const allFlights = await Flight.find({})
+  res.render('flights/index', { 
+    flights: allFlights,  
+  })
 }
--- routes/flights.js
+++ routes/flights.js
@@ -5,6 +5,15 @@ const flightCtrl = require('../controllers/flight');
 // get new listiung with GET, /flights/new
 router.get('/new', flightCtrl.new);
 
+
+
+// POST /flights
+
+router.post('/', flightCtrl.create)
+
+// GET '/' listing.
+router.get('/', flightCtrl.index)
+
 /* GET users listing. */
 // router.get('/', function(req, res, next) {
 //   res.send('respond with a resource');
